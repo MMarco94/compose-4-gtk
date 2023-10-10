9e88ac9dec0f474b5aafcb7e10147857beecf8df
Marco <marco.marangoni1@gmail.com>
2023-10-10 20:51:56 +0100

    Helper functions for Picture

--- src/main/kotlin/io/github/mmarco94/compose/gtk/components/Picture.kt
+++ src/main/kotlin/io/github/mmarco94/compose/gtk/components/Picture.kt
@@ -9,10 +9,63 @@ import io.github.mmarco94.compose.modifier.Modifier
 import org.gnome.gdk.Paintable
 import org.gnome.gtk.ContentFit
 import org.gnome.gtk.Picture
+import java.io.File
 
 sealed interface Image {
     data class File(val file: org.gnome.gio.File) : Image
     data class Paintable(val paintable: org.gnome.gdk.Paintable) : Image
+}
+
+@Composable
+fun Picture(
+    image: File,
+    modifier: Modifier = Modifier,
+    alternativeText: String? = null,
+    canShrink: Boolean = true,
+    contentFit: ContentFit = ContentFit.CONTAIN,
+) {
+    Picture(
+        image = Image.File(org.gnome.gio.File.newForPath(image.absolutePath)),
+        modifier = modifier,
+        alternativeText = alternativeText,
+        canShrink = canShrink,
+        contentFit = contentFit
+    )
+}
+
+@Composable
+fun Picture(
+    image: org.gnome.gio.File,
+    modifier: Modifier = Modifier,
+    alternativeText: String? = null,
+    canShrink: Boolean = true,
+    contentFit: ContentFit = ContentFit.CONTAIN,
+) {
+    Picture(
+        image = Image.File(image),
+        modifier = modifier,
+        alternativeText = alternativeText,
+        canShrink = canShrink,
+        contentFit = contentFit
+    )
+}
+
+
+@Composable
+fun Picture(
+    image: Paintable,
+    modifier: Modifier = Modifier,
+    alternativeText: String? = null,
+    canShrink: Boolean = true,
+    contentFit: ContentFit = ContentFit.CONTAIN,
+) {
+    Picture(
+        image = Image.Paintable(image),
+        modifier = modifier,
+        alternativeText = alternativeText,
+        canShrink = canShrink,
+        contentFit = contentFit
+    )
 }
 
 @Composable
