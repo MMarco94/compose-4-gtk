ba91cf84c36a8655947e7b3c5a8a0f3f22028480
Sébastien Guillemin <sebastien.guillemin@gmail.com>
2025-04-10 22:22:11 +0200

    Add StatusPAge

---
+++ src/test/kotlin/StatusPage.kt
@@ -0,0 +1,50 @@
+import androidx.compose.runtime.*
+import io.github.mmarco94.compose.adw.application
+import io.github.mmarco94.compose.adw.components.ApplicationWindow
+import io.github.mmarco94.compose.adw.components.HeaderBar
+import io.github.mmarco94.compose.adw.components.StatusPage
+import io.github.mmarco94.compose.gtk.components.Image
+import io.github.mmarco94.compose.gtk.components.VerticalBox
+import io.github.mmarco94.compose.modifier.Modifier
+import io.github.mmarco94.compose.modifier.cssClasses
+import io.github.mmarco94.compose.modifier.expandVertically
+import org.gnome.adw.SpinnerPaintable
+
+fun main(args: Array<String>) {
+    application("my.example.HelloApp", args) {
+        ApplicationWindow(
+            application,
+            "StatusPage",
+            onClose = ::exitApplication,
+            defaultWidth = 800,
+            defaultHeight = 900,
+        ) {
+            val spinner: Image.Paintable? by remember { mutableStateOf(Image.Paintable(SpinnerPaintable.builder().build())) }
+
+            VerticalBox {
+                HeaderBar()
+                StatusPage(
+                    title = "Loading",
+                    description = "Please wait...",
+                    paintable = spinner,
+                    modifier = Modifier.expandVertically(),
+                )
+                StatusPage(
+                    title = "No Results Found",
+                    description = "Try a different search",
+                    iconName = "system-search-symbolic",
+                    modifier = Modifier
+                        .expandVertically()
+                        .cssClasses("compact"),
+                )
+                StatusPage(
+                    title = "No Results Found",
+                    description = "Try a different search",
+                    iconName = "system-search-symbolic",
+                    modifier = Modifier
+                        .expandVertically(),
+                )
+            }
+        }
+    }
+}
