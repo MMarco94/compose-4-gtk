e68a380ce361606b1430c9525879d36138e210bd
Thomas Lavoie <lavoiethomas17@gmail.com>
2025-04-17 15:33:30 -0400

    Add Banner with example

---
+++ src/main/kotlin/io/github/mmarco94/compose/adw/components/Banner.kt
@@ -0,0 +1,40 @@
+package io.github.mmarco94.compose.adw.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeNode
+import io.github.jwharm.javagi.gobject.SignalConnection
+import io.github.mmarco94.compose.GtkApplier
+import io.github.mmarco94.compose.LeafComposeNode
+import org.gnome.adw.Banner
+
+private class AdwBannerComposeNode(
+    gObject: Banner
+) : LeafComposeNode<Banner>(gObject) {
+    var onButtonClicked: SignalConnection<Banner.ButtonClickedCallback>? = null
+}
+
+@Composable
+fun Banner(
+    title: String? = null,
+    buttonLabel: String? = null,
+    revealed: Boolean = true,
+    useMarkup: Boolean = false,
+    onButtonClicked: (() -> Unit)? = null,
+) {
+    ComposeNode<AdwBannerComposeNode, GtkApplier>({
+        AdwBannerComposeNode(Banner.builder().build())
+    }) {
+        set(title) { this.gObject.title = it }
+        set(buttonLabel) { this.gObject.buttonLabel = it }
+        set(revealed) { this.gObject.revealed = it }
+        set(useMarkup) { this.gObject.useMarkup = it }
+        set(onButtonClicked) {
+            this.onButtonClicked?.disconnect()
+            if (onButtonClicked != null) {
+                this.onButtonClicked = this.gObject.onButtonClicked {
+                    onButtonClicked()
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/Banner.kt
@@ -0,0 +1,44 @@
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import io.github.mmarco94.compose.adw.application
+import io.github.mmarco94.compose.adw.components.ApplicationWindow
+import io.github.mmarco94.compose.adw.components.Banner
+import io.github.mmarco94.compose.adw.components.HeaderBar
+import io.github.mmarco94.compose.adw.components.HorizontalClamp
+import io.github.mmarco94.compose.gtk.components.ToggleButton
+import io.github.mmarco94.compose.gtk.components.VerticalBox
+import io.github.mmarco94.compose.modifier.Modifier
+import io.github.mmarco94.compose.modifier.alignment
+import io.github.mmarco94.compose.modifier.expand
+import org.gnome.gtk.Align
+
+fun main(args: Array<String>) {
+    val bannerTitle =
+        "<i><span foreground=\"red\" size=\"x-large\">Hell</span><span foreground=\"green\" size=\"x-large\">o wo</span><span foreground=\"blue\" size=\"x-large\">rld!</span></i>"
+
+    application("my.example.HelloApp", args) {
+        ApplicationWindow("Test", onClose = ::exitApplication, defaultWidth = 600, defaultHeight = 400) {
+            val isRevealed = remember { mutableStateOf(false) }
+
+            VerticalBox {
+                HeaderBar()
+                Banner(
+                    title = bannerTitle,
+                    "Hide me!",
+                    revealed = isRevealed.value,
+                    useMarkup = true,
+                    onButtonClicked = {
+                        isRevealed.value = false
+                    })
+
+                HorizontalClamp(modifier = Modifier.expand(true)) {
+                    ToggleButton(
+                        modifier = Modifier.alignment(Align.CENTER),
+                        label = "${if (isRevealed.value) "Hide" else "Reveal"} the banner",
+                        active = isRevealed.value,
+                        toggled = { isRevealed.value = !isRevealed.value })
+                }
+            }
+        }
+    }
+}
