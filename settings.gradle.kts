87a6dae2ce003e9d24c229ae3919aceb22dc72a0
Marco <marco.marangoni1@gmail.com>
2025-05-03 12:52:22 +0100

    Clean settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,1 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-
-    plugins {
-        id("org.jetbrains.kotlin.jvm")
-        id("org.jetbrains.compose")
-        id("org.jetbrains.kotlin.plugin.compose")
-        id("org.jetbrains.dokka")
-        id("me.qoomon.git-versioning")
-    }
-}
-
 rootProject.name = "compose-4-gtk"
