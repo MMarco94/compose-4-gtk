c43fd5422cf1daf842fe3669df69aae02fd2df09
Marco <marco.marangoni1@gmail.com>
2025-05-03 12:42:27 +0100

    Remove unused datetime library

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation(libs.kotlin.logging)
     implementation(libs.slf4j.api)
 
-    testImplementation(libs.kotlinx.datetime)
     testImplementation(libs.slf4j.simple)
 }
 
